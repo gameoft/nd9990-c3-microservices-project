bdaa26a24a7b9d05485c9956f6bd3c4a06d53a79
riccardo <dellasantar@gmail.com>
2020-05-26 15:05:18 +0200

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=dbudagramds
+export POSTGRES_PASSWORD=dbudagramds
+export POSTGRES_HOST=dbudagramds.cf2jfo1zouy7.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagramdsdev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
